386e54a75cac0120cd95f3890c47e6ad3e5aa60f
samuelhz96 <77164950+samuelhz96@users.noreply.github.com>
2021-01-15 02:32:27 +0100

    Update numerical green state.R

--- numerical green state.R
+++ numerical green state.R
@@ -63,8 +63,23 @@ e_g_outer
 persp(gamma,A,e_g_outer,zlim = c(0.00001,0.00015),ticktype = 'detailed')
 
 
-# It looks like this is almost independent of gamma and A values. Maybe an approach with mu and eta varying explains more
+# It looks like this is almost independent of gamma and A values. Maybe an approach with mu and eta varying explains more:
+# vary eta and mu
+gamma <- 0.5; A <- 2; b <- 0.96
+e_g <- seq(0.0000001,0.0001,length.out = 100)
+mu <- seq(0.01,0.99,length.out = 30)
+eta <- seq(1,2,length.out = 30)
+e_g_outer <- matrix(rep(0,30^2),ncol = 30,nrow = 30)
 
+for(i in 1:length(mu)){
+  for(j in 1:length(eta)){
+    e_g_outer[i,j] <- e_g[which.max(log(((1)/(1+b))*(1+A*e_g^(gamma)-e_g)) + 
+                                      b*log(((b)/(1+b))*(1+A*e_g^(gamma)-e_g)) - 
+                                      (1+b)*((e_g*N)/mu[i])^(eta[j]))]
+  }
+}
+persp(mu,eta,e_g_outer,ticktype = 'detailed', theta = -45, phi=20)
 
+# clearly the varuation in mu explains the most change, while eta does appear to have a greater influence than A or gamma
 
 
